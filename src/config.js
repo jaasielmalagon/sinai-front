b23908ed5fbc62055824a71eb58fe6eaf3d58903
jaasielmalagon <jaasiel.mendez@alumno.uttehuacan.edu.mx>
2019-05-25 13:36:50 -0500

    creando clientes con firebase/CREDENCIALES CAMBIADAS

--- src/config.js
+++ src/config.js
@@ -1,15 +1,25 @@
 import Firebase from "firebase";
 var conf = {
-    apiKey: "AIzaSyBaAFsuMMRTIkgKdhKky2XzIo7vC-FNeew",
-    authDomain: "sinai-b9d68.firebaseapp.com",
-    databaseURL: "https://sinai-b9d68.firebaseio.com",
-    projectId: "sinai-b9d68",
-    storageBucket: "sinai-b9d68.appspot.com",
-    messagingSenderId: "295694321019",
-    appId: "1:295694321019:web:b0f3d3b9be532099"
+   // JAASIEL
+   //  apiKey: "AIzaSyBaAFsuMMRTIkgKdhKky2XzIo7vC-FNeew",
+   //  authDomain: "sinai-b9d68.firebaseapp.com",
+   //  databaseURL: "https://sinai-b9d68.firebaseio.com",
+   //  projectId: "sinai-b9d68",
+   //  storageBucket: "sinai-b9d68.appspot.com",
+   //  messagingSenderId: "295694321019",
+   //  appId: "1:295694321019:web:b0f3d3b9be532099"
+
+   // SINAI
+   apiKey: "AIzaSyCuyEPDRJ1wZgHKM-vNSDa9etD0O8BAYGU",
+   authDomain: "sinai-28c9b.firebaseapp.com",
+   databaseURL: "https://sinai-28c9b.firebaseio.com",
+   projectId: "sinai-28c9b",
+   storageBucket: "sinai-28c9b.appspot.com",
+   messagingSenderId: "892358533325",
+   appId: "1:892358533325:web:4e769667d934852e"
 }
 Firebase.initializeApp(conf);
 let db = Firebase.database();
 export default {
-   db : db 
+   db: db
 }
